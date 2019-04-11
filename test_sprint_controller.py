7e025902d3f15bc6e1e798a39a545eda068156f8
ZichengWang <31023128+NEPDream@users.noreply.github.com>
2019-04-11 14:50:26 -0400

    latest unit test code for sprint ctroller

--- test_sprint_controller.py
+++ test_sprint_controller.py
@@ -1,28 +1,16 @@
 import unittest
 import json
 from app import db, app
-from requests.auth import _basic_auth_str
-from app.api_module.helpers import string_generator
 import warnings
 
-# from app.api_module.models import Role
-# import jwt
-
 '''
-To start this test, we should first use postman to create at least one company and two different projects in the database
-And we must have no sprint under project_id=2 because one project_id can only have one sprint
-we should have one sprint with following information in the database:
-json_sprint = {"name": "sprint_test_name",
-                "start_date": "2019-02-20 00:00:00",
-                "due_date": "2019-02-28 00:00:00",
-                "comment": "sprint_test_comment",
-                "project_id": 1}
+To run test successfully, plz delete your local app.db first
 '''
-
 
 class SprintTest(unittest.TestCase):
     """
-      Sprints Test Case
+      Sprint Test Case
+      by Zicheng Wang
     """
 
     def setUp(self):
@@ -31,68 +19,149 @@ class SprintTest(unittest.TestCase):
         """
         self.app = app
         self.client = self.app.test_client
-        self.user_admin = {
-            'username': 'wzc@email.com',
-            'password': 'wzc123',
-            'token': ''
-        }
-        self.user_not_admin = {
-            'username': 'role_test@email.com',
-            'password': 'role123',
-            'token': ''
-        }
-
         with self.app.app_context():
             # create all tables
             db.create_all()
 
-    def test_admin_create_sprint(self):
-        """ test role creation """
-        warnings.simplefilter("ignore")
-        request_token = self.client().get('/api/login/',
-                                          headers={'Content-Type': 'application/json',
-                                                   'Authorization': _basic_auth_str(self.user_admin.get('username'),
-                                                                                    self.user_admin.get('password'))})
-        json_data = json.loads(request_token.data)
-        self.user_admin['token'] = json_data.get('token')
-        json_sprint = {"name": string_generator(),
-                       "start_date": "2019-02-20 00:00:00",
-                       "due_date": "2019-02-28 00:00:00",
-                       "comment": string_generator(),
-                       "project_id": 2}
-        request_sprint_admin_create = self.client().post('/api/sprint/', headers={'Content-Type': 'application/json',
-                                                                                  'x-access-token': self.user_admin[
-                                                                                      'token']},
-                                                         data=json.dumps(json_sprint))
-        json_data = json.loads(request_sprint_admin_create.data)
-        self.assertTrue(json_data.get('message'))
-        self.assertEqual(json_data.get('message'), 'New sprint created!')
-        self.assertEqual(request_sprint_admin_create.status_code, 200)
+        # Create tests users
+        json_admin_user = {"name": "wzc_admin", "email": "wzc_admin@email.com",
+                           "password": "wzc", "admin": True,
+                           "profile": "Software Engineer",
+                           "skills": ["java", "C#", "Python"]}
 
-    def test_admin_get_all_sprints(self):
-        """ test to get all roles """
+        self.client().post('/api/user/',
+                           headers={'Content-Type': 'application/json',
+                                    'x-access-token': ''},
+                           data=json.dumps(json_admin_user))
+
+        self.user_admin = {
+            'username': json_admin_user['email'],
+            'password': json_admin_user['password'],
+            'token': ''
+        }
+
+        # Get tokens
+        json_login_admin = {
+            "username": self.user_admin['username'],
+            "password": self.user_admin['password']
+        }
+        res_login_admin = self.client().post('/api/login',
+                                             headers={'Content-Type': 'application/json',
+                                                      'Authorization': 'Basic'},
+                                             data=json.dumps(json_login_admin))
+        json_data_login_admin = json.loads(res_login_admin.data)
+        self.user_admin['token'] = json_data_login_admin.get('token')
+
+        # Create a company
+        json_test_company = {'name': 'company_test_name1', 'comment': 'company_test_comment1'}
+        self.client().post('/api/company/', headers={'Content-Type': 'application/json',
+                                                     'x-access-token': self.user_admin[
+                                                         'token']},
+                           data=json.dumps(json_test_company))
+
+        # Create a project
+        json_test_project = {
+            "name": "project_test_name1",
+            "start_date": "2019-04-8 00:00:00",
+            "due_date": "2019-5-30 00:00:00",
+            "comment": "project_test_comment1",
+            "company_id": 1
+        }
+        self.client().post('/api/project/', headers={'Content-Type': 'application/json',
+                                                     'x-access-token': self.user_admin[
+                                                         'token']},
+                           data=json.dumps(json_test_project))
+
+        # create a sprint
+        json_test_sprint1 = {
+            "name": "sprint_test_name1",
+            "start_date": "2019-04-1 00:00:00",
+            "due_date": "2019-04-15 00:00:00",
+            "comment": "sprint_test_comment1",
+            "project_id": 1
+        }
+        self.client().post('/api/sprint/', headers={'Content-Type': 'application/json',
+                                                    'x-access-token': self.user_admin[
+                                                        'token']},
+                           data=json.dumps(json_test_sprint1))
+
+    def test_a_create_sprint(self):
         warnings.simplefilter("ignore")
-        request_token = self.client().get('/api/login/',
-                                          headers={'Content-Type': 'application/json',
-                                                   'Authorization': _basic_auth_str(self.user_admin.get('username'),
-                                                                                    self.user_admin.get('password'))})
-        json_data = json.loads(request_token.data)
-        self.user_admin['token'] = json_data.get('token')
-        request_admin_get_sprint = self.client().get('/api/sprint/', headers={'Content-Type': 'application/json',
-                                                                              'x-access-token': self.user_admin.get(
-                                                                                  'token')})
-        get_data = json.loads(request_admin_get_sprint.data)
-        result = get_data.get('sprints')[0]['name']
-        self.assertEqual(result, 'sprint_test_name')
+        json_test_sprint2 = {
+            "name": "sprint_test_name2",
+            "start_date": "2019-04-1 00:00:00",
+            "due_date": "2019-04-15 00:00:00",
+            "comment": "sprint_test_comment2",
+            "project_id": 1
+        }
+        request_sprint_create = self.client().post('/api/sprint/', headers={'Content-Type': 'application/json',
+                                                                              'x-access-token': self.user_admin[
+                                                                                  'token']},
+                                                       data=json.dumps(json_test_sprint2))
+        json_data_test_create_sprint = json.loads(request_sprint_create.data)
+        self.assertEqual(json_data_test_create_sprint.get('message'), 'New sprint created!')
+
+    def test_b_update_sprint(self):
+        warnings.simplefilter("ignore")
+        json_test_update_sprint = {
+            "name": "sprint_test_name3",
+            "start_date": "2019-05-1 00:00:00",
+            "due_date": "2019-05-15 00:00:00"
+        }
+        request_update_sprint = self.client().put('/api/sprint/1/', headers={'Content-Type': 'application/json',
+                                                                            'x-access-token': self.user_admin[
+                                                                                'token']},
+                                                   data=json.dumps(json_test_update_sprint))
+        json_data_test_update_sprint = json.loads(request_update_sprint.data)
+        print(json_data_test_update_sprint)
+        self.assertEqual(json_data_test_update_sprint.get('message'), 'sprint updated!')
+
+    def test_c_get_all_sprints(self):
+        warnings.simplefilter("ignore")
+        request_get_all_sprints = self.client().get('/api/sprint/', headers={'Content-Type': 'application/json',
+                                                                             'x-access-token': self.user_admin[
+                                                                                 'token']}  )
+        json_data_test_update_sprint = json.loads(request_get_all_sprints.data)
+        res=json_data_test_update_sprint['sprints'][0]['name']
+        self.assertEqual(res, 'sprint_test_name1')
+
+    def test_d_get_first_sprint(self):
+        warnings.simplefilter("ignore")
+        request_get_1st_sprints = self.client().get('/api/sprint/1/', headers={'Content-Type': 'application/json',
+                                                                             'x-access-token': self.user_admin[
+                                                                                 'token']}  )
+        json_data_get_1st_sprint = json.loads(request_get_1st_sprints.data)
+        res=json_data_get_1st_sprint['sprint']['name']
+        self.assertEqual(res, 'sprint_test_name1')
+
+    def test_e_get_not_exist_sprint(self):
+        warnings.simplefilter("ignore")
+        request_get_non_exist_sprints = self.client().get('/api/sprint/2/', headers={'Content-Type': 'application/json',
+                                                                             'x-access-token': self.user_admin[
+                                                                                 'token']}  )
+        json_data_get_non_exist_sprint = json.loads(request_get_non_exist_sprints.data)
+        res=json_data_get_non_exist_sprint['message']
+        self.assertEqual(res, 'No sprint found!')
+
+    def test_f_get_sprint_from_project1(self):
+        warnings.simplefilter("ignore")
+        request_get_sprint_from_project1 = self.client().get('/api/sprint/project/1/', headers={'Content-Type': 'application/json',
+                                                                             'x-access-token': self.user_admin[
+                                                                                 'token']}  )
+        json_data_sprint_from_project1 = json.loads(request_get_sprint_from_project1.data)
+        res=json_data_sprint_from_project1
+        self.assertEqual(res, 'sprint_test_name1')
+
+
 
     def tearDown(self):
         """
         Tear Down
         """
         with self.app.app_context():
-            print("ending the app testing")
-            # db.session.remove()
-            # db.drop_all()
+            # Drop all tables
+            db.session.remove()
+            db.drop_all()
 
 
 if __name__ == '__main__':
