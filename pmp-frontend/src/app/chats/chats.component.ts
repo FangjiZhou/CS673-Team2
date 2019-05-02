1f2964f052d8458fd92e78b817f7caaf63445ddb
@Cyril <fotso.cyril@gmail.com>
2019-05-02 17:53:51 -0400

    adding the overall updates, stats, homepage...

--- pmp-frontend/src/app/chats/chats.component.ts
+++ pmp-frontend/src/app/chats/chats.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
 import {message} from '../models/message';
 import {action} from '../models/action';
 import {event} from '../models/event';
@@ -10,6 +10,9 @@ import { ScrollToService, ScrollToConfigOptions } from '@nicky-lenaers/ngx-scrol
 import {AuthService} from '../services/auth.service'
 import { embeddedViewEnd } from '@angular/core/src/render3';
 import { MomentModule } from 'angular2-moment';
+import { fromEvent } from 'rxjs';
+import { switchMap, takeUntil, pairwise } from 'rxjs/operators'
+
 
 
 
@@ -18,8 +21,7 @@ import { MomentModule } from 'angular2-moment';
   templateUrl: './chats.component.html',
   styleUrls: ['./chats.component.scss']
 })
-export class ChatsComponent implements OnInit {
-
+export class ChatsComponent implements OnInit , AfterViewInit{
   action = action;
   messageContent: string;
   ioConnection: any;
@@ -36,11 +38,22 @@ export class ChatsComponent implements OnInit {
   current_room_id : number;
   text_message='';
   current_room_name='';
+  selected_room_name='';
+
+  x = "black";
+  y = 2;
+  w: any;
+  h: any;
+
+  @ViewChild('canvas') public canvas: ElementRef;
+  private cx: CanvasRenderingContext2D;
+  drawingSubscription: any;
+
+  toggled: boolean = false;
 
   constructor(private _auth: AuthService, private modalService: NgbModal, private chatserv: ChatService, private userserv: userserv, private toast: ToastrService, private _scrollToService: ScrollToService) { }
 
   ngOnInit() {
-    // init the globals arrays of list
     this.userserv.listEmployee().subscribe(data => {
       this.employee_list = data['employees']
     });
@@ -58,12 +71,132 @@ export class ChatsComponent implements OnInit {
     this.current_emp_id = this._auth.getUserEmployeeId;
   }
 
+  handleSelection(event) {
+    console.log(event.char);
+    this.text_message += event.char;
+  }
+  
+  public ngAfterViewInit() {
+    const canvasEl: HTMLCanvasElement = this.canvas.nativeElement;
+    this.cx = canvasEl.getContext('2d');
+    this.w = canvasEl.width;
+    this.h = canvasEl.height;
+    this.cx.lineWidth = this.y;
+    this.cx.lineCap = 'round';
+    this.cx.strokeStyle = '#000';
+    this.cx.fillStyle = this.x;
+    this.captureEvents(canvasEl);
+  }
+
+
+  captureEvents(canvasEl: HTMLCanvasElement) {
+    // this will capture all mousedown events from teh canvas element
+    this.drawingSubscription = fromEvent(canvasEl, 'mousedown')
+      .pipe(
+        switchMap(e => {
+          // after a mouse down, we'll record all mouse moves
+          return fromEvent(canvasEl, 'mousemove').pipe(
+            // we'll stop (and unsubscribe) once the user releases the mouse
+            // this will trigger a 'mouseup' event
+            takeUntil(fromEvent(canvasEl, 'mouseup')),
+            // we'll also stop (and unsubscribe) once the mouse leaves the canvas (mouseleave event)
+            takeUntil(fromEvent(canvasEl, 'mouseleave')),
+            // pairwise lets us get the previous value to draw a line from
+            // the previous point to the current point
+            pairwise()
+          );
+        })
+      )
+      .subscribe((res: [MouseEvent, MouseEvent]) => {
+        const rect = canvasEl.getBoundingClientRect();
+
+        // previous and current position with the offset
+        const prevPos = {
+          x: res[0].clientX - rect.left,
+          y: res[0].clientY - rect.top
+        };
+
+        const currentPos = {
+          x: res[1].clientX - rect.left,
+          y: res[1].clientY - rect.top
+        };
+
+        // this method we'll implement soon to do the actual drawing
+        this.drawOnCanvas(prevPos, currentPos);
+      });
+  }
+
+  private drawOnCanvas(prevPos: { x: number, y: number }, currentPos: { x: number, y: number }) {
+    if (!this.cx) { return; }
+
+    this.cx.beginPath();
+    this.cx.strokeStyle = this.x;
+    this.cx.fillStyle = this.x;
+    this.cx.lineWidth = this.y;
+
+    if (prevPos) {
+      this.cx.moveTo(prevPos.x, prevPos.y); // from
+      this.cx.lineTo(currentPos.x, currentPos.y);
+      this.cx.stroke();
+    }
+  }
+  
+
+  erase() {
+      // var m = confirm("Want to clear");
+      // if (m) {
+      this.cx.clearRect(0, 0, this.w, this.h);
+      document.getElementById("canvasimg").style.display = "none";
+      // }
+  }
+
+ save() {
+    // this.drawingSubscription.unsubscribe();
+    document.getElementById("canvasimg").style.border = "2px solid";
+    const canvas: HTMLCanvasElement = this.canvas.nativeElement;
+    var dataURL = canvas.toDataURL();
+    console.log('data image : ', dataURL)
+    document.getElementById("canvasimg")['src'] = dataURL;
+    document.getElementById("canvasimg").style.display = "inline";
+}
+  
+  color(color) {
+    switch (color) {
+        case "green":
+        this.x = "green";
+            break;
+        case "blue":
+        this.x = "blue";
+            break;
+        case "red":
+        this.x = "red";
+            break;
+        case "yellow":
+        this.x = "yellow";
+            break;
+        case "orange":
+        this.x = "orange";
+            break;
+        case "black":
+        this.x = "black";
+            break;
+        case "white":
+        this.x = "white";
+            break;
+    }
+    if (this.x == "white") this.y = 14;
+    else this.y = 2;
+    console.log(this.x);
+
+  }
+
   private initIoConnection(): void {
     this.chatserv.initSocket();
     this.ioConnection = this.chatserv.onMessage()
       .subscribe((message: any) => {
         // console.log('message', message);
         // find the room and push the new message in the message array
+        console.log(message);
         console.log('finding the corresponding room');
 
         this.list_messages.filter(room => room['room_id']==message['chatroom_id'])[0]['room_messages'].push(message);
@@ -115,18 +248,18 @@ export class ChatsComponent implements OnInit {
       });
   }
 
-
   public setVisible(room_id:number, room_name?:string):void{
     if (room_id == null){
       this.list_messages.forEach(element => {
         this.room_display[element.room_id] = false;
       });
       this.current_room_name ='';
+      this.selected_room_name='';
     }else{
       this.list_messages.forEach(element => {
         if(room_id == element.room_id){
           this.room_display[element.room_id] = true; 
-          this.current_room_name = ' >>> '+room_name;
+          this.current_room_name = room_name;
         }else{
           this.room_display[element.room_id] = false;
         }
@@ -175,7 +308,6 @@ export class ChatsComponent implements OnInit {
       target: String(div_id)
     };
     this._scrollToService.scrollTo(config);
-    // console.log('je scrolle', div_id);
   }
 
   private getDismissReason(reason: any): string {
@@ -316,9 +448,10 @@ export class ChatsComponent implements OnInit {
     modal.dismiss('Submitting the form');
   }
 
-  activateCharRoom(room_id:number, room_name?:string){
+  activateCharRoom(room_id:number, room_name?:string, real_room_nane?:string){
     this.setVisible(room_id, room_name);
     this.current_room_id = room_id;
+    this.selected_room_name = real_room_nane;
     console.log(room_id, this.room_display);
     // scroll to the botom
     this.triggerScrollTo('room_'+String(room_id));
@@ -333,6 +466,7 @@ export class ChatsComponent implements OnInit {
         employee_id :this.current_emp_id,
         room_id : room_id,
         room : room_name,
+        type : 'text',
         message : this.text_message,
         sending_date : date_msg.getFullYear().toString()+'-'+String(date_msg.getMonth()+1)+'-'+date_msg.getDay().toString()+' '+date_msg.getHours().toString()+':'+date_msg.getMinutes().toString()+':'+date_msg.getSeconds().toString()
       }
@@ -343,16 +477,42 @@ export class ChatsComponent implements OnInit {
     }
   }
 
-  keyPress(event: any, room_id:number, room_name:string) {
-    var code = (event.keyCode ? event.keyCode : event.which);
-    if (code == 13) { //Enter keycode
-      if (event.shiftKey){
-        console.log('you press shift toooo, so avoid sending the message')
-      }else{
-        this.send_new_message(room_id, room_name);
-      }
+  sendPictureMsg(){
+    const canvas: HTMLCanvasElement = this.canvas.nativeElement;
+    var dataURL = canvas.toDataURL();
+    if(this.current_room_id && this.selected_room_name!=''){
+      var date_msg = new Date();   
+      var message ={
+        employee_id :this.current_emp_id,
+        room_id : this.current_room_id,
+        room : this.selected_room_name,
+        type : 'img',
+        message : dataURL,
+        sending_date : date_msg.getFullYear().toString()+'-'+String(date_msg.getMonth()+1)+'-'+date_msg.getDay().toString()+' '+date_msg.getHours().toString()+':'+date_msg.getMinutes().toString()+':'+date_msg.getSeconds().toString()
+       }
+      console.log('post message',message)
+      this.chatserv.post_in_room('text', message);
+    }else{
+      alert('no chat room selected');
     }
   }
+  
+  keyPress(event?: any, room_id?:number, room_name?:string) {
+    if (event != null){
+      var code = (event.keyCode ? event.keyCode : event.which);
+      if (code == 13) { //Enter keycode
+        if (event.shiftKey){
+          console.log('you press shift toooo, so avoid sending the message')
+        }else{
+          this.send_new_message(room_id, room_name);
+        }
+      }
+    }else{
+      this.send_new_message(room_id, room_name);
+    }
+    
+  }
+
 
 
 }
